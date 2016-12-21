6326c088d26da6deeb75070149943fde8178b16b
Etienne Peiniau <etienne.peiniau@gmail.com>
2016-12-21 18:28:26 +0100

    Finishing chapter 4

--- src/main/scala-2.12/exercises/Exercises4.scala
+++ src/main/scala-2.12/exercises/Exercises4.scala
@@ -8,6 +8,8 @@ package exercises
   */
 object Exercises4 {
 
+  // Option
+
   // exercise 4.1
 
   sealed trait Option[+A] {
@@ -36,8 +38,52 @@ object Exercises4 {
 
   // exercice 4.2
 
-  def mean(xs: Seq[Double]): Option[Double] = xs.foldLeft(None: Option[Double])((op, value) => op.map(a => a + value).orElse(Some(value))).map(sum => sum / xs.length)
+  def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
 
   def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap (m => mean(xs map (x => math.pow(x - m, 2))))
+
+  // exercise 4.3
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = a.flatMap(v1 => b.map(v2 => f(v1, v2)))
+
+  // exercise 4.4
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a.foldRight[Option[List[A]]](Some(Nil))((op, opl) => opl.flatMap(l => op.map(a => a :: l)))
+
+  // exercise 4.5
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a.foldRight[Option[List[B]]](Some(Nil))((a, opl) => opl.flatMap(l => f(a).map(b => b :: l)))
+
+  // Either
+
+  sealed trait Either[+E, +A] {
+
+    // exercise 4.6
+
+    def map[B](f: A => B): Either[E, B] = flatMap(a => Right(f(a)))
+
+    def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
+      case Right(a) => f(a)
+      case Left(e) => Left(e)
+    }
+
+    def orElse[EE >: E, B >: A](b: => Either[EE, B]): Either[EE, B] = this match {
+      case Left(e) => b
+      case r => r;
+    }
+
+    def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C] = flatMap(va => b.map(vb => f(va, vb)))
+
+  }
+
+  case class Left[+E](value: E) extends Either[E, Nothing]
+
+  case class Right[+A](value: A) extends Either[Nothing, A]
+
+  // exercise 4.7
+
+  def sequenceEither[E, A](es: List[Either[E, A]]): Either[E, List[A]] = es.foldRight[Either[E, List[A]]](Right(Nil))((ea, es) => ea.map2(es)(_ :: _))
+
+  def traverseEither[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] = as.foldRight[Either[E, List[B]]](Right(Nil))((a, es) => f(a).map2(es)(_ :: _))
 
 }
--- src/test/scala-2.12/exercises/Exercises4Spec.scala
+++ src/test/scala-2.12/exercises/Exercises4Spec.scala
@@ -64,9 +64,145 @@ class Exercises4Spec extends FlatSpec {
   }
 
   "mean of List(1,2,3)" should "return Some(2)" in {
-    assert(mean(List(1,2,3)) == Some(2))
+    assert(mean(List(1, 2, 3)) == Some(2))
   }
 
+  "variance of Nil" should "return None" in {
+    assert(variance(Nil) == None)
+  }
 
+  "variance of List(1,2,3,4,5)" should "return Some(2)" in {
+    assert(variance(List(1, 2, 3, 4, 5)) == Some(2))
+  }
+
+  // exercise 4.3
+
+  "map2 of None, None and (a,b) => a + b" should "return None" in {
+    assert(map2(None: Option[Int], None)((a, b) => a + b) == None)
+  }
+
+  "map2 of None, Some(1) and (a,b) => a + b" should "return None" in {
+    assert(map2(None: Option[Int], Some(1))((a, b) => a + b) == None)
+  }
+
+  "map2 of Some(1), None and (a,b) => a + b" should "return None" in {
+    assert(map2(Some(1), None)((a, b) => a + b) == None)
+  }
+
+  "map2 of Some(1), Some(2) and (a,b) => a + b" should "return Some(3)" in {
+    assert(map2(Some(1), Some(2))((a, b) => a + b) == Some(3))
+  }
+
+  // exerciqe 4.4
+
+  "sequence of List(None)" should "return None" in {
+    assert(sequence(List(None)) == None)
+  }
+
+  "sequence of List(Some(1),Some(2),Some(3))" should "return Some(List(1,2,3))" in {
+    assert(sequence(List(Some(1), Some(2), Some(3))) == Some(List(1, 2, 3)))
+  }
+
+  "sequence of List(Some(1),None,Some(2))" should "return None" in {
+    assert(sequence(List(Some(1), None, Some(2))) == None)
+  }
+
+  // exercise 4.5
+
+  "traverse of List(1,2,3) and a => None" should "return None" in {
+    assert(traverse(List(1,2,3))(a => None) == None)
+  }
+
+  "traverse of Nil and a => None" should "return Some(Nil)" in {
+    assert(traverse(Nil)(a => None) == Some(Nil))
+  }
+
+  "traverse of List(1,2,3) and a => Some(a)" should "return Some(List(1,2,3))" in {
+    assert(traverse(List(1,2,3))(a => Some(a)) == Some(List(1,2,3)))
+  }
+
+  "traverse of Nil and a => Some(a)" should "return Some(Nil)" in {
+    assert(traverse(Nil)(a => Some(a)) == Some(Nil))
+  }
+
+  "traverse of List(1,2,3) and a => if (a < 2) Some(a) else None" should "return None" in {
+    assert(traverse(List(1,2,3))(a => if (a < 2) Some(a) else None) == None)
+  }
+
+  // exercise 4.6
+
+  "map Left(1) and a => 3" should "return Left(1)" in {
+    assert(Left(1).map { a => 3 } == Left(1))
+  }
+
+  "map Right(1) and a => a * 2" should "return Right(2)" in {
+    assert(Right(1).map { a => a * 2 } == Right(2))
+  }
+
+  "flatMap Left(1) and a => Left(3)" should "return Left(1)" in {
+    assert(Left(1).flatMap { a => Left(3) } == Left(1))
+  }
+
+  "flatMap Right(1) and a => Right(a * 2)" should "return Right(2)" in {
+    assert(Right(1).flatMap { a => Right(a * 2) } == Right(2))
+  }
+
+  "orElse Left(1) and Right(2)" should "return Right(2)" in {
+    assert(Left(1).orElse(Right(2)) == Right(2))
+  }
+
+  "orElse Right(2) and Right(1)" should "return Right(2)" in {
+    assert(Right(2).orElse(Right(1)) == Right(2))
+  }
+
+  "map2 of Left(1), Left(2) and (a,b) => 3" should "return Left(1)" in {
+    assert(Left(1).map2(Left(2))((a, b) => 3) == Left(1))
+  }
+
+  "map2 of Left(1), Right(2) and (a,b) => 3" should "return Left(1)" in {
+    assert(Left(1).map2(Right(2))((a, b) => 3) == Left(1))
+  }
+
+  "map2 of Right(1), Left(2) and (a,b) => 3" should "return None" in {
+    assert(Right(1).map2(Left(2))((a, b) => 3) == Left(2))
+  }
+
+  "map2 of Right(1), Right(2) and (a,b) => 3" should "return Right(3)" in {
+    assert(Right(1).map2(Right(2))((a, b) => 3) == Right(3))
+  }
+
+  // exerciqe 4.7
+
+  "sequenceEither of List(Left(1), Left(2))" should "return Left(1)" in {
+    assert(sequenceEither(List(Left(1), Left(2))) == Left(1))
+  }
+
+  "sequenceEither of List(Right(1),Right(2),Right(3))" should "return Right(List(1,2,3))" in {
+    assert(sequenceEither(List(Right(1), Right(2), Right(3))) == Right(List(1, 2, 3)))
+  }
+
+  "sequenceEither of List(Right(1),Left(1),Right(2))" should "return Left(1)" in {
+    assert(sequenceEither(List(Right(1), Left(1), Right(2))) == Left(1))
+  }
+
+  "traverseEither of List(1,2,3) and a => Left(a)" should "return Left(1)" in {
+    assert(traverseEither(List(1,2,3))(a => Left(a)) == Left(1))
+  }
+
+  "traverseEither of Nil and a => Left(a)" should "return Right(Nil)" in {
+    assert(traverseEither(Nil)(a => Left(a)) == Right(Nil))
+  }
+
+  "traverseEither of List(1,2,3) and a => Right(a)" should "return Right(List(1,2,3))" in {
+    assert(traverseEither(List(1,2,3))(a => Right(a)) == Right(List(1,2,3)))
+  }
+
+  "traverseEither of Nil and a => Right(a)" should "return Right(Nil)" in {
+    assert(traverseEither(Nil)(a => Right(a)) == Right(Nil))
+  }
+
+  "traverseEither of List(1,2,3) and a => if (a < 2) Right(a) else Left(a)" should "return Left(2)" in {
+    assert(traverseEither(List(1,2,3))(a => if (a < 2) Right(a) else Left(a)) == Left(2))
+  }
 
 }
